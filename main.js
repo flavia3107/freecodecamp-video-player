70845e150d122915d7091fcc5b4e655b845ae8ac
Flavia Kosti <flaviakosti@gmail.com>
2025-03-28 18:39:37 -0400

    VideoPlayer - P3

--- main.js
+++ main.js
@@ -1,24 +1,101 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+"use strict";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const playNpauseBtn = document.querySelector("#play-pause");
+const video = document.querySelector("video");
+const rewindBtn = document.querySelector("#rewind");
+const fastForwardBtn = document.querySelector("#fast-forward");
+const volumeBtn = document.querySelector("#volume");
+const progressIndicator = document.querySelector("#progress-indicator");
+const progessBar = document.querySelector("#progress-bar");
+const fullscreen = document.getElementById("fullscreen");
+const playButton = document.getElementById("playButton");
 
-setupCounter(document.querySelector('#counter'))
+function playNpauseFn() {
+  video.paused ? video.play() : video.pause();
+}
+
+function updatePlayNPauseIcon() {
+  const icon = playNpauseBtn.querySelector("i");
+  icon.textContent = "";
+
+  icon.textContent = video.paused ? "play_arrow" : "paused";
+  playButton.style.display = video.paused ? "block" : "none";
+
+}
+
+function rewindNforwardFn(type) {
+  video.currentTime += type === "rewind" ? -10 : 10;
+}
+
+function muteNunmuteFn() {
+  video.muted = video.muted ? false : true;
+}
+
+function updateVolumeIcon() {
+  const icon = volumeBtn.querySelector("i");
+  icon.textContent = "";
+  icon.textContent = video.muted ? "volume_off" : "volume_up";
+}
+
+function updateProgress() {
+  const progressPercentage = (video.currentTime / video.duration) * 100;
+
+  progressIndicator.style.width = `${progressPercentage}%`;
+}
+
+function seekingFn(e) {
+  const updatedTime = (e.offsetX / progessBar.offsetWidth) * video.duration;
+
+  video.currentTime = updatedTime;
+}
+
+// PLAY AND PAUSE FUNCTIONALITY
+video.addEventListener("play", updatePlayNPauseIcon);
+video.addEventListener("click", playNpauseFn);
+video.addEventListener("pause", updatePlayNPauseIcon);
+playNpauseBtn.addEventListener("click", playNpauseFn);
+playButton.addEventListener("click", playNpauseFn);
+
+// REWIND AND FAST FORWARD
+rewindBtn.addEventListener("click", () => rewindNforwardFn("rewind"));
+fastForwardBtn.addEventListener("click", () => rewindNforwardFn("forward"));
+
+// MUTE AND UNMUTE
+video.addEventListener("volumechange", updateVolumeIcon);
+volumeBtn.addEventListener("click", muteNunmuteFn);
+
+// PROGRESS
+video.addEventListener("timeupdate", updateProgress);
+
+// SEEKING
+let mouseIsDown = false;
+
+progessBar.addEventListener("mousedown", () => (mouseIsDown = true));
+progessBar.addEventListener("mouseup", () => (mouseIsDown = false));
+progessBar.addEventListener("click", seekingFn);
+progessBar.addEventListener("mousemove", (e) => mouseIsDown && seekingFn);
+
+// KEYBOARD NAVIGATIONS
+window.addEventListener("keyup", (e) => {
+  if (e.code === "Space") {
+    playNpauseFn();
+  } else if (e.code === "ArrowLeft") {
+    rewindNforwardFn("rewind");
+  } else if (e.code === "ArrowRight") {
+    rewindNforwardFn("forward");
+  } else {
+    return;
+  }
+});
+
+fullscreen.addEventListener("click", () => {
+  if (video.requestFullscreen) {
+    video.requestFullscreen();
+  } else if (video.mozRequestFullScreen) {
+    video.mozRequestFullScreen();
+  } else if (video.webkitRequestFullscreen) {
+    video.webkitRequestFullscreen();
+  } else if (video.msRequestFullscreen) {
+    video.msRequestFullscreen();
+  }
+});
